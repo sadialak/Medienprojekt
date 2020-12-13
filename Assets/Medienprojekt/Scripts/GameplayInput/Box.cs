6759aa78935db520a97194235db3a4a49d0784d7
s8gnscha <s8gnscha@stud.uni-saarland.de>
2020-12-13 18:08:55 +0100

    Input aufgesetzt und Hauptmenü verändert sowie Start vom eigentlichen Game (Fehler bei Hauptmenü: Background Button wird nicht richtig angezeigt)

---
+++ Assets/Medienprojekt/Scripts/GameplayInput/Box.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Box : MonoBehaviour
+{
+    private PlayerControls controls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls = new PlayerControls();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Medienprojekt/Scripts/GameplayInput/PlayerControls.cs
@@ -0,0 +1,286 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Medienprojekt/Scripts/GameplayInput/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""63472f68-f357-4e83-a656-2a8a7d4c54fd"",
+            ""actions"": [
+                {
+                    ""name"": ""XSmash"",
+                    ""type"": ""Button"",
+                    ""id"": ""4d95c747-9d57-4394-80de-75a79cf6d9cd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""ASmash"",
+                    ""type"": ""Button"",
+                    ""id"": ""e1f8375e-0d93-445c-8e0e-cb0a9d814fc5"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""BSmash"",
+                    ""type"": ""Button"",
+                    ""id"": ""c9198d69-cab4-4d00-a1e5-109dc5c90d5e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""YSmash"",
+                    ""type"": ""Button"",
+                    ""id"": ""49ea8200-77d9-41e1-b81e-42d13fe4b217"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""bae44542-31f1-4dcf-958c-0f2a7824cb7c"",
+                    ""path"": ""<HID::usb gamepad           >/trigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""XSmash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ba28115d-2bbd-4e9a-976b-1e0d3a8d0fcb"",
+                    ""path"": ""<HID::usb gamepad           >/button2"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ASmash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""2404aef0-15e6-4834-9e8a-4f7293aadd5f"",
+                    ""path"": ""<HID::usb gamepad           >/button3"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""BSmash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c53d4729-dae0-4565-9a38-e606f5329036"",
+                    ""path"": ""<HID::usb gamepad           >/button4"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""YSmash"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""PressStart"",
+            ""id"": ""b0ba3b83-3f58-4ae1-acae-983790233abe"",
+            ""actions"": [
+                {
+                    ""name"": ""Start"",
+                    ""type"": ""Button"",
+                    ""id"": ""1f1353de-18c5-4ab9-a865-44f3049bd38a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""09a8513a-44e0-435e-a60d-59e6988a3ca2"",
+                    ""path"": ""<HID::usb gamepad           >/button10"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Start"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_XSmash = m_Gameplay.FindAction("XSmash", throwIfNotFound: true);
+        m_Gameplay_ASmash = m_Gameplay.FindAction("ASmash", throwIfNotFound: true);
+        m_Gameplay_BSmash = m_Gameplay.FindAction("BSmash", throwIfNotFound: true);
+        m_Gameplay_YSmash = m_Gameplay.FindAction("YSmash", throwIfNotFound: true);
+        // PressStart
+        m_PressStart = asset.FindActionMap("PressStart", throwIfNotFound: true);
+        m_PressStart_Start = m_PressStart.FindAction("Start", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_XSmash;
+    private readonly InputAction m_Gameplay_ASmash;
+    private readonly InputAction m_Gameplay_BSmash;
+    private readonly InputAction m_Gameplay_YSmash;
+    public struct GameplayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GameplayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @XSmash => m_Wrapper.m_Gameplay_XSmash;
+        public InputAction @ASmash => m_Wrapper.m_Gameplay_ASmash;
+        public InputAction @BSmash => m_Wrapper.m_Gameplay_BSmash;
+        public InputAction @YSmash => m_Wrapper.m_Gameplay_YSmash;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @XSmash.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnXSmash;
+                @XSmash.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnXSmash;
+                @XSmash.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnXSmash;
+                @ASmash.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnASmash;
+                @ASmash.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnASmash;
+                @ASmash.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnASmash;
+                @BSmash.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnBSmash;
+                @BSmash.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnBSmash;
+                @BSmash.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnBSmash;
+                @YSmash.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnYSmash;
+                @YSmash.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnYSmash;
+                @YSmash.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnYSmash;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @XSmash.started += instance.OnXSmash;
+                @XSmash.performed += instance.OnXSmash;
+                @XSmash.canceled += instance.OnXSmash;
+                @ASmash.started += instance.OnASmash;
+                @ASmash.performed += instance.OnASmash;
+                @ASmash.canceled += instance.OnASmash;
+                @BSmash.started += instance.OnBSmash;
+                @BSmash.performed += instance.OnBSmash;
+                @BSmash.canceled += instance.OnBSmash;
+                @YSmash.started += instance.OnYSmash;
+                @YSmash.performed += instance.OnYSmash;
+                @YSmash.canceled += instance.OnYSmash;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+
+    // PressStart
+    private readonly InputActionMap m_PressStart;
+    private IPressStartActions m_PressStartActionsCallbackInterface;
+    private readonly InputAction m_PressStart_Start;
+    public struct PressStartActions
+    {
+        private @PlayerControls m_Wrapper;
+        public PressStartActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Start => m_Wrapper.m_PressStart_Start;
+        public InputActionMap Get() { return m_Wrapper.m_PressStart; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PressStartActions set) { return set.Get(); }
+        public void SetCallbacks(IPressStartActions instance)
+        {
+            if (m_Wrapper.m_PressStartActionsCallbackInterface != null)
+            {
+                @Start.started -= m_Wrapper.m_PressStartActionsCallbackInterface.OnStart;
+                @Start.performed -= m_Wrapper.m_PressStartActionsCallbackInterface.OnStart;
+                @Start.canceled -= m_Wrapper.m_PressStartActionsCallbackInterface.OnStart;
+            }
+            m_Wrapper.m_PressStartActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Start.started += instance.OnStart;
+                @Start.performed += instance.OnStart;
+                @Start.canceled += instance.OnStart;
+            }
+        }
+    }
+    public PressStartActions @PressStart => new PressStartActions(this);
+    public interface IGameplayActions
+    {
+        void OnXSmash(InputAction.CallbackContext context);
+        void OnASmash(InputAction.CallbackContext context);
+        void OnBSmash(InputAction.CallbackContext context);
+        void OnYSmash(InputAction.CallbackContext context);
+    }
+    public interface IPressStartActions
+    {
+        void OnStart(InputAction.CallbackContext context);
+    }
+}
