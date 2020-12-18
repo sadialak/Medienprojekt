c90c02c6eee8298267e7b50531b23430f131ef27
s8gnscha <s8gnscha@stud.uni-saarland.de>
2020-12-18 13:14:18 +0100

    Stand 18.12.20

---
+++ Assets/Medienprojekt/Scripts/Button/Buttonmove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttonmove : MonoBehaviour
+{
+    public GameObject xButton, yButton, aButton, bButton;
+    private float respawnTime = 3.0f;
+   
+    public int zahl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnButton());
+    }
+
+    IEnumerator SpawnButton()
+    {
+        while (true)
+        {
+           
+            zahl = Random.Range(1,5);
+            if (zahl == 1)
+            {
+                GameObject a = Instantiate(aButton) as GameObject;
+                
+            }
+            if (zahl == 2)
+            {
+                GameObject b = Instantiate(bButton) as GameObject;
+                
+            }
+            if (zahl == 3)
+            {
+                GameObject x = Instantiate(xButton) as GameObject;
+                
+            }
+            if (zahl == 4)
+            {
+                GameObject y = Instantiate(yButton) as GameObject;
+                
+            }
+            yield return new WaitForSeconds(respawnTime);
+        }
+    }
+}
---
+++ Assets/Medienprojekt/Scripts/GameplayInput/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+	private PlayerControls controls;
+	public float respawnTime = 0.5f;
+    void Awake()
+    {
+		controls= new PlayerControls();
+        sprite = this.GetComponent<SpriteRenderer>();
+		controls.Gameplay.XSmash.performed+=ctx =>  StartCoroutine(SetXColor());
+		controls.Gameplay.ASmash.performed+=ctx => StartCoroutine(SetAColor());
+		controls.Gameplay.BSmash.performed+=ctx =>  StartCoroutine(SetBColor());
+		controls.Gameplay.YSmash.performed+=ctx =>  StartCoroutine(SetYColor());
+    }
+
+	IEnumerator SetAColor()
+	{
+	sprite.color=Color.red;
+	yield return new WaitForSeconds(respawnTime);
+	sprite.color=Color.white;
+	
+	}
+
+	IEnumerator SetBColor()
+	{
+	sprite.color=Color.yellow;
+	yield return new WaitForSeconds(respawnTime);
+	sprite.color=Color.white;
+
+	}
+
+	IEnumerator SetXColor()
+	{
+	sprite.color=Color.blue;
+	yield return new WaitForSeconds(respawnTime);
+	sprite.color=Color.white;
+
+	}
+
+	IEnumerator SetYColor()
+	{
+	sprite.color=Color.green;
+	yield return new WaitForSeconds(respawnTime);
+	sprite.color=Color.white;
+
+	}
+
+	void OnEnable(){
+	controls.Gameplay.Enable();
+	}
+
+	void OnDisable(){
+	controls.Gameplay.Disable();
+	}
+    
+}
