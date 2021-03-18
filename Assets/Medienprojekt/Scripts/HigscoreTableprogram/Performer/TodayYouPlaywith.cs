fc77fefd9c6dac7ddd29d2e07fd277b6343ca47b
s8gnscha <s8gnscha@stud.uni-saarland.de>
2021-03-18 17:25:52 +0100

    Maingameänderung4 (18.03.21)

--- Assets/Medienprojekt/Scripts/HigscoreTableprogram/Performer/TodayYouPlaywith.cs
+++ Assets/Medienprojekt/Scripts/HigscoreTableprogram/Performer/TodayYouPlaywith.cs
@@ -18,7 +18,7 @@ public class TodayYouPlaywith : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text ="Today you'll play with: "+PlayerPrefs.GetString("Performer");
+        text.text ="Today you will dance through: "+PlayerPrefs.GetString("Performer");
         
     }
 }
